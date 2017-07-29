b2eb7057eb389ce8c6dfd9aa498025b8f9ca211c
gtktsc <gtktsc@gmail.com>
2017-07-29 23:22:21 +0200

    game logic change

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@
     var canvas = document.getElementById('canvas'),
         context = canvas.getContext('2d');
 
-    var head;
+    var head =[];
+    var color = Math.floor(Math.random() * (4));
+    var backgroundColor = "silver";
+    var shape;
     var tileX=[];
     var tileOccupied=[];
     var animation;
@@ -13,31 +16,145 @@
     var sumY =Math.floor(window.innerHeight / snakeSize);
     
     function restart(){
-        head = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
-         0,
-          "silver" ,
-           snakeSize ,
-            -1,
-            Math.floor(Math.random() * (4)));
+        setShape();
+
+
 
         var nr=0;
         for(var nrX=0;nrX<sumX;nrX++){
             for(var nrY=0;nrY<sumY;nrY++){
             tileX[nr]= new Rectangle (snakeSize*nrX,
                                         snakeSize*nrY,
-                                        "white",
+                                        backgroundColor,
                                         snakeSize,
-                                        nr,
-                                        0);
+                                        nr);
             tileX[nr].direction="none";
             nr++;
             };
         };
 
-
         clearInterval(animation);
         animation = setInterval(draw , 400);
     };
+
+    function setShape(){
+        shape=Math.floor(Math.random() * (4));
+        color = "#"+((1<<24)*Math.random()|0).toString(16);
+        switch(shape){
+            case 0:
+                head=[];
+                head[0] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[1] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[2] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                snakeSize,
+                color ,
+                snakeSize ,
+                -1);
+                head[3] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize*2,
+                +snakeSize,
+                color ,
+                snakeSize ,
+                -1);
+                break;
+            case 1:
+                head=[];
+                head[0] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[1] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[2] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+ snakeSize*2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+            break;
+            case 2:
+                head=[];
+                head[0] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[1] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[2] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+ snakeSize*2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[3] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
+                snakeSize,
+                color ,
+                snakeSize ,
+                -1);
+                break;
+            case 3:
+                head=[];
+                head[0] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[1] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[2] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+ snakeSize*2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[3] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                snakeSize,
+                color ,
+                snakeSize ,
+                -1);
+                break;
+            case 4:
+                head=[];
+                head[0] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[1] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                0,
+                color ,
+                snakeSize ,
+                -1);
+                head[2] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2,
+                snakeSize,
+                color ,
+                snakeSize ,
+                -1);
+                head[3] = new Rectangle ( (Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2+snakeSize,
+                snakeSize,
+                color ,
+                snakeSize ,
+                -1);
+                break;
+        };
+    };
+
+
 
     function resizeCanvas() {
         sumX =Math.floor(window.innerWidth / snakeSize);
@@ -50,100 +167,80 @@
         context.beginPath();
         context.clearRect(0 , 0 , window.innerWidth , window.innerHeight);
         context.rect(0 , 0 , window.innerWidth , window.innerHeight);
-        context.fillStyle = 'white';
+        context.fillStyle = backgroundColor;
         context.fill();
         context.closePath();
+
+
+
 
         for(var nr=0;nr<tileX.length;nr++){
             tileX[nr].draw();
         };
-
-        head.draw();
+        for(var tile in head){
+            if(head[tile]!=undefined && (head[tile].y > window.innerHeight-head[tile].size * 2 || touching(head[tile])  )){
+                stop();
+            };
+        }
+        for(var tile in head){
+            head[tile].draw();
+        };
     };
     window.addEventListener('resize' , resizeCanvas , false);
     window.addEventListener('keydown', function(event) {
         switch (event.keyCode) {
             case 68: // D
             case 39: // down
-                head.direction !== "left" ? head.direction = "right" : head.directory=head.directory;
+                for(var tile in head){
+                    head[tile].direction !== "left" ? head[tile].direction = "right" : head[tile].directory=head[tile].directory;
+                };
             break;
             case 65: // A
             case 37: // left
-                head.direction !== "right" ? head.direction = "left" : head.directory=head.directory;
+            for(var tile in head){
+                    head[tile].direction !== "right" ? head[tile].direction = "left" : head[tile].directory=head[tile].directory;
+            };
             break;
             case 83: // S
             case 40: // down
-                head.direction !== "up" ? head.direction = "down" : head.directory=head.directory;
+            for(var tile in head){
+                head[tile].direction !== "up" ? head[tile].direction = "down" : head[tile].directory=head[tile].directory;
+            };
             break;
         };
     });
 
     function stop(){
-        head.direction="none";
-        for(var nr=0;nr<tileX.length;nr++){
-            if(tileX[nr].x==head.x&&tileX[nr].y==head.y){
-                tileOccupied[tileOccupied.length] = tileX[nr];
-                tileX[nr].color=head.color;
-                head.color = "#"+((1<<24)*Math.random()|0).toString(16);
-                head.x=(Math.floor(window.innerWidth / snakeSize)/2) * snakeSize -snakeSize/2;
-                head.y= 0;
-                head.shape=Math.floor(Math.random() * (4));
-                head.direction = "down";
+
+        for(var tile in head){
+            for(var nr=0;nr<tileX.length;nr++){
+                if(tileX[nr].x==head[tile].x&&tileX[nr].y==head[tile].y){
+                    tileX[nr].color=head[tile].color;
+                    tileOccupied[tileOccupied.length] = tileX[nr];
+                };
             };
         };
+        setShape();
     };
 
-    function touching(){
+    function touching(who){
         for(var nr=0;nr<tileOccupied.length;nr++){
-            switch(head.shape){
-                case 0:
-                    //x
-                    if(tileOccupied[nr].x==head.x && tileOccupied[nr].y==head.y+head.size){
-                        return true;
-                    };
-                    break;
-                case 1:
-                    //xxx
-                    if(tileOccupied[nr].x==head.x||tileOccupied[nr].x==head.x+head.size||tileOccupied[nr].x==head.x+head.size*2 
-                        && tileOccupied[nr].y==head.y+head.size){
-                        return true;
-                    };
-                break;
-                case 2:
-                    //xxx
-                    // x
-
-                break;
-                case 3:
-                    //xxx
-                    //x
-
-                break;
-                case 4:
-                    //xx
-                    //xx
-
-                break;
+            if(tileOccupied[nr].x==who.x && tileOccupied[nr].y==who.y+who.size){
+                return true;
             };
         };
         return false;
     };
 
-    function Rectangle(x , y , color , size , number, shape){
+    function Rectangle(x , y , color , size , number){
         this.x = x;
         this.y = y;
-        this.shape = shape;
+        this.shape = 0;
         this.direction = "down";
         this.number = number;
         this.size = size;
         this.color = color;
         this.draw = function() {
-            if(this.number<0){
-                if(this.y > window.innerHeight-this.size * 2 || touching()  ){
-                    stop();
-                };
-            };
-
             if(this.direction!="none"){
                 switch(this.direction){
                     case "down":
@@ -165,42 +262,7 @@
             };
             context.beginPath();
             context.fillStyle = this.color;
-            switch(this.shape){
-                case 0:
-                    //x
-                    context.rect(this.x , this.y , this.size , this.size);
-                break;
-                case 1:
-                    //xxx
-                    context.rect(this.x , this.y , this.size , this.size);
-                    context.rect(this.x +this.size, this.y , this.size , this.size);
-                    context.rect(this.x +this.size*2, this.y , this.size , this.size);
-                break;
-                case 2:
-                    //xxx
-                    // x
-                    context.rect(this.x , this.y , this.size , this.size);
-                    context.rect(this.x + this.size, this.y , this.size , this.size);
-                    context.rect(this.x+ this.size*2, this.y , this.size , this.size);
-                    context.rect(this.x+ this.size, this.y +this.size, this.size , this.size);
-                break;
-                case 3:
-                    //xxx
-                    //x
-                    context.rect(this.x, this.y , this.size , this.size);
-                    context.rect(this.x+ this.size, this.y , this.size , this.size);
-                    context.rect(this.x+ this.size*2, this.y , this.size , this.size);
-                    context.rect(this.x, this.y + this.size, this.size , this.size);
-                break;
-                case 4:
-                    //xx
-                    //xx
-                    context.rect(this.x, this.y , this.size , this.size);
-                    context.rect(this.x, this.y + this.size, this.size , this.size);
-                    context.rect(this.x + this.size, this.y , this.size , this.size);
-                    context.rect(this.x + this.size, this.y + this.size, this.size , this.size);
-                break;
-            };
+            context.rect(this.x , this.y , this.size , this.size);
             context.fill();
             context.closePath();
         };
